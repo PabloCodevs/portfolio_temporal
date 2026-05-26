a5526f22aa69375a1da89b5bc62ce0ac3ef4ad3a
PabloJGV <caveroescudero.pablo@jgvaldemora.org>
2026-05-26 12:05:49 +0200

    Portfolio terminado

--- proyects/Objetos literales/main.js
+++ proyects/Objetos literales/main.js
@@ -1,82 +1,83 @@
-// Objeto Goku simple
-
 const goku = {
-    nombre: "Goku",
     x: 0,
     y: 0,
-    vel: 50,
+    vel: 20,
     elemento: null,
-    
-    init: function() {
+
+    init() {
         this.elemento = document.getElementById("goku");
         this.actualizar();
     },
-    
-    mover: function(dirX, dirY) {
-        this.x = this.x + (dirX * this.vel);
-        this.y = this.y + (dirY * this.vel);
-        
-        let mapa = document.querySelector(".mapa");
-        let maxW = mapa.offsetWidth;
-        let maxH = mapa.offsetHeight;
-        
+
+    mover(dx, dy) {
+        const mapa = document.querySelector(".mapa");
+
+        this.x += dx * this.vel;
+        this.y += dy * this.vel;
+
+        const maxX = mapa.clientWidth - 50;
+        const maxY = mapa.clientHeight - 50;
+
         if (this.x < 0) this.x = 0;
-        if (this.x > maxW - 50) this.x = maxW - 50;
+        if (this.x > maxX) this.x = maxX;
+
         if (this.y < 0) this.y = 0;
-        if (this.y > maxH - 50) this.y = maxH - 50;
-        
-        this.mostrarEfecto("Mov!");
+        if (this.y > maxY) this.y = maxY;
+
         this.actualizar();
+        this.efecto("Movimiento");
     },
-    
-    teletransportar: function() {
-        let mapa = document.querySelector(".mapa");
-        this.x = Math.random() * (mapa.offsetWidth - 50);
-        this.y = Math.random() * (mapa.offsetHeight - 50);
-        
-        this.mostrarEfecto("TELE!");
+
+    teletransportar() {
+        const mapa = document.querySelector(".mapa");
+
+        this.x = Math.random() * (mapa.clientWidth - 50);
+        this.y = Math.random() * (mapa.clientHeight - 50);
+
         this.actualizar();
+        this.efecto("Teletransporte");
     },
-    
-    actualizar: function() {
+
+    actualizar() {
         this.elemento.style.left = this.x + "px";
         this.elemento.style.top = this.y + "px";
-        
-        document.getElementById("posX").innerHTML = Math.round(this.x);
-        document.getElementById("posY").innerHTML = Math.round(this.y);
+
+        document.getElementById("posX").textContent = Math.round(this.x);
+        document.getElementById("posY").textContent = Math.round(this.y);
     },
-    
-    mostrarEfecto: function(msg) {
-        let div = document.createElement("div");
-        div.className = "efecto";
-        div.innerHTML = msg;
-        document.getElementById("efectos").appendChild(div);
-        
-        setTimeout(function() {
-            div.remove();
-        }, 1000);
+
+    efecto(texto) {
+        const cont = document.getElementById("efectos");
+
+        const div = document.createElement("div");
+        div.classList.add("efecto");
+        div.textContent = texto;
+
+        cont.appendChild(div);
+
+        setTimeout(() => div.remove(), 1000);
     }
 };
 
-// Iniciar
 goku.init();
 
-// Controles
-document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowUp") {
-        goku.mover(0, -1);
-    }
-    if (e.key === "ArrowDown") {
-        goku.mover(0, 1);
-    }
-    if (e.key === "ArrowLeft") {
-        goku.mover(-1, 0);
-    }
-    if (e.key === "ArrowRight") {
-        goku.mover(1, 0);
-    }
-    if (e.key === " ") {
-        e.preventDefault();
-        goku.teletransportar();
+document.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case "ArrowUp":
+            goku.mover(0, -1);
+            break;
+        case "ArrowDown":
+            goku.mover(0, 1);
+            break;
+        case "ArrowLeft":
+            goku.mover(-1, 0);
+            break;
+        case "ArrowRight":
+            goku.mover(1, 0);
+            break;
+        case " ":
+            e.preventDefault();
+            goku.teletransportar();
+            break;
     }
 });
